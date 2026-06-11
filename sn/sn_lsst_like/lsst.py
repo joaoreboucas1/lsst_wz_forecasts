ab9b18f2511ca988a3e8127e10eeeebb743e466e
diogohf <diogohenriquefrsz@gmail.com>
2026-06-11 10:49:36 -0700

    Add dmb statistical error to SN covariance

--- sn/sn_lsst_like/lsst.py
+++ sn/sn_lsst_like/lsst.py
@@ -10,10 +10,13 @@ class lsst(PantheonPlus):
     """
 
     def configure(self):
-        self.pre_vars = 0.  # diagonal component
+        # DHFS note: 
+        # For a total LSST positive definite cov, 
+        # MUST add per-SN stat errors on top of syst cov matrix!
+        self.pre_vars = self.dmb**2 
 
     def _read_data_file(self, data_file):
-        file_cols = ['zcmb', 'mb']
-        self.cols = ['zcmb', 'mag']
+        file_cols = ['zcmb', 'zhel', 'mb', 'dmb']
+        self.cols = ['zcmb', 'zhel', 'mag', 'dmb']
         self._read_cols(data_file, file_cols)
         self.zhel = self.zcmb
